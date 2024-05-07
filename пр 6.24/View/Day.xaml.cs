8018e5ccb48b6531d2a6f8d8e3a8d7b12d961bcf
ronetel <isip_e.a.kuznetsov@mpt.ru>
2024-05-07 12:13:36 +0300

    _

---
+++ пр 6.24/View/Day.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using пр_6._24.ViewModel;
+
+namespace пр_6._24
+{
+    /// <summary>
+    /// Логика взаимодействия для Day.xaml
+    /// </summary>
+    public partial class Day : UserControl
+    {
+        public Day()
+        {
+            InitializeComponent();
+            DataContext = new DayViewModel();
+        }
+    }
+}
